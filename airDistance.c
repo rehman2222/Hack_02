8f45a112716bcf47f61fe374704a1c9772e3f11d
Rehman <rk03229975701@gmail.com>
2023-10-19 00:20:43 -0700

    add some varibles and constant

--- airDistance.c
+++ airDistance.c
@@ -13,5 +13,30 @@ double degree_to_Radain(double value);
 
 int main()
 {
-    
+ 
+  double lon1,p1,lon2,p2;
+  double distance = 0; delta = 0;
+  const int Radius = 6371;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
 }
