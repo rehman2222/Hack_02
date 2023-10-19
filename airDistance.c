2767d903515ab189f17320bb0fef8a05af16cbb6
Rehman <rk03229975701@gmail.com>
2023-10-19 00:16:43 -0700

    add name, date , preproccessor directry

---
+++ airDistance.c
@@ -0,0 +1,17 @@
+/* 
+Author Abdur rehman 
+Date 2023/10/19
+
+This program calculate the distanc with the help of 
+longtitude.
+*/ 
+
+#include<stdio.h>
+#include<math.h>
+
+double degree_to_Radain(double value);
+
+int main()
+{
+    
+}
